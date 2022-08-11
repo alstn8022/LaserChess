e390c10e6ddea89fb56c750a440e18ed706c2b0a
alstn <72611608+alstn8022@users.noreply.github.com>
2022-08-11 15:19:04 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,125 @@
+x1, y1, alignedFrom = [x for x in input().split()]
+x2, y2 = [int(x) for x in input().split()]
+
+x1 = int(x1)
+y1 = int(y2)
+pos = input()
+x2 = int(input())
+y2 = int(input())
+
+if pos == "d" :
+    if y1 > y2:
+        if x1 > x2:
+            print("2")
+        elif x1 < x2:
+            print("2")
+        elif x1 == x2:
+            print("3")
+        else:
+            print("-1")
+if pos == "d" :
+        if y1 < y2:
+            if x1 > x2:
+                print("1")
+            elif x1 < x2:
+                print("1")
+            elif x1 == x2:
+                print("3")
+            else:
+                print("-1")
+if pos == "d" :
+        if y1 == y2:
+            if x1 > x2:
+                print("2")
+            elif x1 < x2:
+                print("2")
+            else:
+                print("-1")
+
+if pos == "u" :
+    if y1 > y2:
+        if x1 > x2:
+            print("1")
+        elif x1 < x2:
+            print("1")
+        elif x1 == x2:
+            print("3")
+        else:
+            print("-1")
+if pos == "u" :
+        if y1 < y2:
+            if x1 > x2:
+                print("2")
+            elif x1 < x2:
+                print("2")
+            elif x1 == x2:
+                print("3")
+            else:
+                print("-1")
+if pos == "u" :
+        if y1 == y2:
+            if x1 > x2:
+                print("2")
+            elif x1 < x2:
+                print("2")
+            else:
+                print("-1")
+
+if pos == "l" :
+    if y1 > y2:
+        if x1 > x2:
+            print("1")
+        elif x1 < x2:
+            print("2")
+        elif x1 == x2:
+            print("2")
+        else:
+            print("-1")
+if pos == "l" :
+    if y1 < y2:
+        if x1 > x2:
+            print("1")
+        elif x1 < x2:
+            print("2")
+        elif x1 == x2:
+            print("2")
+        else:
+            print("-1")
+if pos == "l" :
+    if y1 == y2:
+        if x1 > x2:
+            print("3")
+        elif x1 < x2:
+            print("3")
+        else:
+            print("-1")
+            
+if pos == "r" :
+    if y1 > y2:
+        if x1 > x2:
+            print("2")
+        elif x1 < x2:
+            print("1")
+        elif x1 == x2:
+            print("2")
+        else:
+            print("-1")
+if pos == "r" :
+    if y1 < y2:
+        if x1 > x2:
+            print("2")
+        elif x1 < x2:
+            print("1")
+        elif x1 == x2:
+            print("2")
+        else:
+            print("-1")
+if pos == "r" :
+    if y1 == y2:
+        if x1 > x2:
+            print("3")
+        elif x1 < x2:
+            print("3")
+        else:
+            print("-1")
+       
