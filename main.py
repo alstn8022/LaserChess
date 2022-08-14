487075abd4dd085b96db985aeeaf593b37568071
alstn8022 <pkalstn2018@gmail.com>
2022-08-14 12:07:56 +0900

    Fixed Bugs. But Still May Contain Bugs.

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ x1, y1, alignedFrom = [x for x in input().split()]
 x2, y2 = [int(x) for x in input().split()]
 
 x1 = int(x1)
-y1 = int(y2)
+y1 = int(y1)
 pos = input()
-x2 = int(input())
-y2 = int(input())
+x2 = int(x2)
+y2 = int(y2)
 
-if pos == "d" :
+if pos == "d":
     if y1 > y2:
         if x1 > x2:
             print("2")
@@ -17,26 +17,28 @@ if pos == "d" :
             print("3")
         else:
             print("-1")
-if pos == "d" :
-        if y1 < y2:
-            if x1 > x2:
-                print("1")
-            elif x1 < x2:
-                print("1")
-            elif x1 == x2:
-                print("3")
-            else:
-                print("-1")
-if pos == "d" :
-        if y1 = y2:
-            if x1 > x2:
-                print("2")
-            elif x1 < x2:
-                print("2")
-            else:
-                print("-1")
 
-if pos == "u" :
+if pos == "d":
+    if y1 < y2:
+        if x1 > x2:
+            print("1")
+        elif x1 < x2:
+            print("1")
+        elif x1 == x2:
+            print("3")
+        else:
+            print("-1")
+
+if pos == "d":
+    if y1 == x2:
+        if x1 > x2:
+            print("2")
+        elif x1 < x2:
+            print("2")
+        else:
+            print("-1")
+
+if pos == "u":
     if y1 > y2:
         if x1 > x2:
             print("1")
@@ -46,26 +48,28 @@ if pos == "u" :
             print("3")
         else:
             print("-1")
-if pos == "u" :
-        if y1 < y2:
-            if x1 > x2:
-                print("2")
-            elif x1 < x2:
-                print("2")
-            elif x1 == x2:
-                print("3")
-            else:
-                print("-1")
-if pos == "u" :
-        if y1 = y2:
-            if x1 > x2:
-                print("2")
-            elif x1 < x2:
-                print("2")
-            else:
-                print("-1")
 
-if pos == "l" :
+if pos == "u":
+    if y1 < y2:
+        if x1 > x2:
+            print("2")
+        elif x1 < x2:
+            print("2")
+        elif x1 == x2:
+            print("3")
+        else:
+            print("-1")
+
+if pos == "u":
+    if y1 == y2:
+        if x1 > x2:
+            print("2")
+        elif x1 < x2:
+            print("2")
+        else:
+            print("-1")
+
+if pos == "l":
     if y1 > y2:
         if x1 > x2:
             print("1")
@@ -75,7 +79,8 @@ if pos == "l" :
             print("2")
         else:
             print("-1")
-if pos == "l" :
+
+if pos == "l":
     if y1 < y2:
         if x1 > x2:
             print("1")
@@ -85,8 +90,9 @@ if pos == "l" :
             print("2")
         else:
             print("-1")
-if pos == "l" :
-    if y1 = y2:
+
+if pos == "l":
+    if y1 == y2:
         if x1 > x2:
             print("3")
         elif x1 < x2:
@@ -94,7 +100,7 @@ if pos == "l" :
         else:
             print("-1")
             
-if pos == "r" :
+if pos == "r":
     if y1 > y2:
         if x1 > x2:
             print("2")
@@ -104,7 +110,8 @@ if pos == "r" :
             print("2")
         else:
             print("-1")
-if pos == "r" :
+
+if pos == "r":
     if y1 < y2:
         if x1 > x2:
             print("2")
@@ -114,12 +121,12 @@ if pos == "r" :
             print("2")
         else:
             print("-1")
-if pos == "r" :
-    if y1 = y2:
+
+if pos == "r":
+    if y1 == y2:
         if x1 > x2:
             print("3")
         elif x1 < x2:
             print("3")
         else:
             print("-1")
-       
